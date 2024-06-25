f6a2a78d50304cf93538e4732cc3df8d41caaf90
bsteuart <145496337+bsteuart@users.noreply.github.com>
2024-06-25 09:11:39 -0600

    Add files via upload

---
+++ textadventures.py
@@ -0,0 +1,15 @@
+# Project idea: Text-based adventure game
+# 
+# Our story is about one Christmas Eve, all of the lights go out at the North Pole. Santa soon discovers that all of the reindeer have been infected with a disease that makes them
+# hyper-aggressive and have an unsatiable bloodlust. Santa must travel to many different locations in the North Pole to gather all of the ingredients for the antidote.
+
+def santa_room():
+    # ....
+    something = 0
+
+def workshop():
+    # ....
+    something = 0
+
+def stables():
+    something = 0
