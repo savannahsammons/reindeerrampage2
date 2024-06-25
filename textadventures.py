e7f30bd137857ff597d4f3ff7c4530e58868a812
bsteuart <145496337+bsteuart@users.noreply.github.com>
2024-06-25 09:26:36 -0600

    Update textadventures.py
    added intro function

--- textadventures.py
+++ textadventures.py
@@ -2,6 +2,19 @@
 # 
 # Our story is about one Christmas Eve, all of the lights go out at the North Pole. Santa soon discovers that all of the reindeer have been infected with a disease that makes them
 # hyper-aggressive and have an unsatiable bloodlust. Santa must travel to many different locations in the North Pole to gather all of the ingredients for the antidote.
+ANSI_RESET = "\u001B[0m"
+ANSI_BLACK = "\u001B[30m"
+ANSI_RED = "\u001B[31m"
+ANSI_GREEN = "\u001B[32m"
+ANSI_YELLOW = "\u001B[33m"
+ANSI_BLUE = "\u001B[34m"
+ANSI_PURPLE = "\u001B[35m"
+ANSI_CYAN = "\u001B[36m"
+ANSI_WHITE = "\u001B[37m"
+
+
+def intro():
+    print(f'{ANSI_RED} yabba dabba {ANSI_RESET})
 
 def santa_room():
     # ....
